464ce77c091f08ea59a8fbfb1c24ae4f6464d67d
Bendekk <daw.bend@gmail.com>
2022-03-20 22:48:11 +0100

    testingv3

---
+++ Test1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace bsk_zadania1
+{
+    [TestClass]
+    public class Test1
+    {
+        public Rail Rail = new Rail();
+        [TestMethod]
+        public void TestRail()
+        {
+            string word = Rail.RailEncrypt(2, "abcde");
+            Console.Write(word);    
+            Assert.AreEqual("acebd", word);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual("a","a");
+        }
+    }
+}
